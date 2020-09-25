dc8af48087e40a1df38d79287d3adf291382f174
lebusini <598189497@qq.com>
2020-09-25 21:22:25 +0800

    set the dpi and the tick

--- plot_parameter_analysis.py
+++ plot_parameter_analysis.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-
+import matplotlib.ticker as ticker
 img_dir = r'C:\python_pycharm_label_test\compared_experiments\segmentation\experiment_sample\parameter_analysis'
 imglist=os.listdir(img_dir)
 def draw_polyline(nparray,savename,x_label,y_label,title_name):
@@ -10,7 +10,7 @@ def draw_polyline(nparray,savename,x_label,y_label,title_name):
     y2=nparray[:,2]
     y3=nparray[:,3]
     y4=nparray[:,4]
-    plt.figure(figsize=(8, 5))
+    plt.figure(figsize=(3.5, 2),dpi=300)
     plt.grid(linestyle="--")  # 设置背景网格线为虚线
     ax = plt.gca()
     ax.spines['top'].set_visible(False)  # 去掉上边框
@@ -21,26 +21,31 @@ def draw_polyline(nparray,savename,x_label,y_label,title_name):
     # plt.plot(x, C, color="red", label="C algorithm", linewidth=1.5)
     # plt.plot(x, D, "r--", label="D algorithm", linewidth=1.5)
 
-    plt.plot(x,y1,label='Precision',linewidth=1,marker='.',color='r')
-    plt.plot(x,y2,label='Recall',linewidth=1,marker='^',color='g')
-    plt.plot(x,y3,label='F1',linewidth=1,marker='3',color='b')
-    plt.plot(x,y4,label='IoU',linewidth=1,marker='*',color='brown')
+    plt.plot(x,y1,label='Precision',linewidth=0.5,marker='.',color='r',markersize=4)
+    plt.plot(x,y2,label='Recall',linewidth=0.5,marker='^',color='g',markersize=4)
+    plt.plot(x,y3,label='F1',linewidth=0.5,marker='3',color='b',markersize=4)
+    plt.plot(x,y4,label='IoU',linewidth=0.5,marker='*',color='brown',markersize=4)
 
     # group_labels = ['dataset1', 'dataset2', 'dataset3', 'dataset4', 'dataset5', ' dataset6', 'dataset7', 'dataset8',
     #                 'dataset9', 'dataset10']  # x轴刻度的标识
-    plt.xticks( fontsize=15, fontweight='bold')  # 默认字体大小为10
-    plt.yticks(fontsize=15, fontweight='bold')
-    plt.title(title_name, fontsize=20, fontweight='bold')  # 默认字体大小为12
-    plt.xlabel(x_label, fontsize=17, fontweight='bold')
-    plt.ylabel(y_label, fontsize=17, fontweight='bold')
+    # plt.xticks(np.arange(min(x), max(x) + 1, 1.0))
+    plt.xticks(fontproperties = 'Times New Roman', fontsize=8, fontweight='bold')  # 默认字体大小为10
+    plt.yticks(fontproperties = 'Times New Roman',fontsize=8, fontweight='bold')
+    plt.title(title_name, fontdict={'family' : 'Times New Roman', 'size': 8,'weight': 'bold'})  # 默认字体大小为12
+    plt.xlabel(x_label, fontdict={'family' : 'Times New Roman', 'size': 8, 'weight': 'bold'})
+    plt.ylabel(y_label, fontdict={'family' : 'Times New Roman', 'size': 8, 'weight': 'bold'})
     plt.xlim(x[0], x[-1])  # 设置x轴的范围
+    ax.xaxis.set_ticks(x[::2])
+    # ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('%0.2f'))
     # plt.ylim(0.5,1)
+    font = {'family': 'Times New Roman', 'weight': 'bold', 'size': 5}
 
     # plt.legend()          #显示各曲线的图例
-    plt.legend(loc=0, numpoints=1)
-    leg = plt.gca().get_legend()
-    ltext = leg.get_texts()
-    plt.setp(ltext, fontsize=17, fontweight='bold')  # 设置图例字体的大小和粗细
+    plt.legend(loc=0,prop=font)
+    # leg = plt.gca().get_legend()
+    # ltext = leg.get_texts()
+    # plt.setp(ltext, fontsize=2, fontweight='bold',fontproperties = 'Times New Roman')  # 设置图例字体的大小和粗细
+    # plt.margins(x=0)
     plt.savefig(savename + '.png')
     plt.savefig(savename+'.svg', format='svg')  # 建议保存为svg格式，再用inkscape转为矢量图emf后插入word中
     plt.close()
@@ -49,21 +54,21 @@ def draw_polyline(nparray,savename,x_label,y_label,title_name):
 for img_name in imglist:
     prename=img_name.split('.')[0]
 
-    # # mu
-    # mu_test_array=np.load(prename+'_mu.npy')
-    # draw_polyline(mu_test_array,prename+'_mu_polyline',r'$\mu$','Score','values of metrics as the change of '+r'$\mu$')
+    # mu
+    mu_test_array=np.load(prename+'_mu.npy')
+    draw_polyline(mu_test_array,prename+'_mu_polyline',r'$\mu$','Score','values of metrics as the change of '+r'$\mu$')
 
-    # #alpha
-    # lmda_test_array=np.load(prename+'_lmda.npy')
-    # draw_polyline(lmda_test_array,prename+'_lmda_polyline',r'$\lambda$','Score','values of metrics as the change of '+r'$\lambda$')
+    #alpha
+    lmda_test_array=np.load(prename+'_lmda.npy')
+    draw_polyline(lmda_test_array,prename+'_lmda_polyline',r'$\lambda$','Score','values of metrics as the change of '+r'$\lambda$')
+
+    #lamda
+    alfa_test_array=np.load(prename+'_alfa.npy')
+    draw_polyline(alfa_test_array,prename+'_alfa_polyline',r'$\alpha$','Score','values of metrics as the change of '+r'$\alpha$')
     #
-    # #lamda
-    # alfa_test_array=np.load(prename+'_alfa.npy')
-    # draw_polyline(alfa_test_array,prename+'_alfa_polyline',r'$\alpha$','Score','values of metrics as the change of '+r'$\alpha$')
-    #
-    #seg_num
-    n_segments_test_array=np.load(prename+'_n_segments.npy')
-    draw_polyline(n_segments_test_array,prename+'_K_polyline','K','Score','values of metrics as the change of K')
+    # #seg_num
+    # n_segments_test_array=np.load(prename+'_n_segments.npy')
+    # draw_polyline(n_segments_test_array,prename+'_K_polyline','K','Score','values of metrics as the change of K')
     #
     # #kernel_size
     # kernel_test_array=np.load(prename+'_kenel.npy')
